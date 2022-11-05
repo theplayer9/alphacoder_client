0ab517b6286ef544adff5ded71ba1c5e605d8387
Om Sharma <omsharma.theplayer11@gmail.com>
2022-11-05 16:31:15 +0530

    Making landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import { useUserAuth } from "./context/userContext";
+import { UserProvider } from "./context/userContext";
+import ProtectedRoute from "./ProtectiveRoute/ProtectedRoute";
 
 function App() {
-  return <div className="App">Hello World</div>;
-}
+  const [token] = useUserAuth();
 
+  return (
+    <Router>
+      <UserProvider>
+        <Routes>
+          {token === "null" || token === undefined ? (
+            <Route path="/" element={<Home />} />
+          ) : (
+            <Route path="/" element={<Navigate replace to="/dashboard" />} />
+          )}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </UserProvider>
+    </Router>
+  );
+}
 export default App;
